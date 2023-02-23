43bdf712c1fae7771a69e24103593270474e3dbe
yyj9529 <garethgates7@naver.com>
2023-02-23 18:41:55 +0900

    connecting MongoDB

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://127.0.0.1:27017/youtube");
+mongoose.set('strictQuery', false);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ connected to DB");
+const handleError = () => console.log("❌ DB Error",error);
+
+db.on("error",handleError);
+db.once("open",handleOpen);
